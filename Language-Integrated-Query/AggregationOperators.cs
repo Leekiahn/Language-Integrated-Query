4d0b13ae20f0b2adaf574d056889cbff5d4a3e4c
Leekiahn <68183471+Leekiahn@users.noreply.github.com>
2025-10-02 19:28:28 +0900

    AggregationOperators.cs 클래스 추가

---
+++ Language-Integrated-Query/AggregationOperators.cs
@@ -0,0 +1,25 @@
+﻿namespace Language_Integrated_Query;
+
+public class AggregationOperators
+{
+    public static void Run(int[] array)
+    {
+        var count = array.Where(n => n % 2 == 0).Count();
+        Console.WriteLine("Count of even numbers: " + count);
+        
+        var sum = array.Sum();
+        Console.WriteLine("Sum of all numbers: " + sum);
+        
+        var average = array.Average();
+        Console.WriteLine("Average of all numbers: " + average);
+        
+        var min = array.Min();
+        Console.WriteLine("Minimum number: " + min);
+        
+        var max = array.Max();
+        Console.WriteLine("Maximum number: " + max);
+        
+        var aggregate = array.Aggregate((a, b) => a * b);
+        Console.WriteLine("Product of all numbers: " + aggregate); // 1*2*3*...*9 = 362880
+    }
+}
