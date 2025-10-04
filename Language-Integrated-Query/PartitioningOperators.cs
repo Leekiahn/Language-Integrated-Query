f4735ace3c7d6496c7c3cf93988432f0a86f44b7
Leekiahn <68183471+Leekiahn@users.noreply.github.com>
2025-10-04 17:35:45 +0900

    PartitioningOperators.cs 분활 Linq 클래스 추가

---
+++ Language-Integrated-Query/PartitioningOperators.cs
@@ -0,0 +1,17 @@
+﻿namespace Language_Integrated_Query;
+
+public class PartitioningOperators
+{
+    public static void Run()
+    {
+        int[] numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var firstThree = numbers.Take(3); // 1, 2, 3
+
+        var skipThree = numbers.Skip(3); // 4, 5, 6
+
+        var takeWhile = numbers.TakeWhile(n => n < 4); // 1, 2, 3
+
+        var skipWhile = numbers.SkipWhile(n => n < 4); // 4, 5, 6
+    }
+}
